a2359e192c0961e36f5d0d0af513e7d89dd26bee
goofi0211 <gcobs2336588@gmail.com>
2020-10-29 17:12:46 +0800

    ncku image processing

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button10 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(122, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(122, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "mean filter";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 162);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(506, 332);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "R channel";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 70);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "G channel";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 99);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "B channel";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 128);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Grayscale";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(122, 70);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "median filter";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(233, 41);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Original";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(233, 70);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "Result";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // chart1
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart1.Legends.Add(legend6);
+            this.chart1.Location = new System.Drawing.Point(541, 194);
+            this.chart1.Name = "chart1";
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(503, 300);
+            this.chart1.TabIndex = 10;
+            this.chart1.Text = "chart1";
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(340, 70);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "threshold";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(340, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 22);
+            this.textBox1.TabIndex = 12;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(464, 39);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 13;
+            this.button11.Text = "Vertical";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(464, 81);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 14;
+            this.button12.Text = "Horizontal";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(464, 128);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(75, 23);
+            this.button13.TabIndex = 15;
+            this.button13.Text = "Combined";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1281, 612);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,394 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap openImg;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //openFileDialog1.InitialDirectory = "C:";
+            openFileDialog1.Filter = "All Files|*.*|Bitmap Files (.bmp)|*.bmp|Jpeg File(.jpg)|*.jpg";
+            // 選擇我們需要開檔的類型
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            { // 如果成功開檔
+                openImg = new Bitmap(openFileDialog1.FileName);
+
+                
+
+                int[,] R_array = new int[openImg.Width, openImg.Height];
+                int[,] G_array = new int[openImg.Width, openImg.Height];
+                int[,] B_array = new int[openImg.Width, openImg.Height];
+                
+                //get the pixel values
+                for (int x = 0; x < openImg.Width; x++)
+                {
+                    for (int y = 0; y < openImg.Height; y++)
+                    {
+                        Color pixelColor = openImg.GetPixel(x, y);
+                        int r = pixelColor.R;
+                        int g = pixelColor.G;
+                        int b = pixelColor.B;
+                    }
+                }
+
+                // 宣告存取影像的 bitmap
+                pictureBox1.Image = openImg;
+                // 讀取的影像展示到 pictureBox
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap image1 = (Bitmap)openImg.Clone(); //make a copy
+            for (int x = 0; x < openImg.Width; x++)
+            {
+                for (int y = 0; y < openImg.Height; y++)
+                {
+                    //get the r value
+                    Color pixelColor = image1.GetPixel(x, y); 
+                    Color newColor = Color.FromArgb(pixelColor.R, 0, 0);
+                    image1.SetPixel(x, y, newColor);
+                }
+            }
+            pictureBox1.Image = image1;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Bitmap image1 = (Bitmap)openImg.Clone(); //make a copy
+            for (int x = 0; x < openImg.Width; x++)
+            {
+                for (int y = 0; y < openImg.Height; y++)
+                {
+                    //get the G value
+                    Color pixelColor = image1.GetPixel(x, y);
+                    Color newColor = Color.FromArgb(0, pixelColor.G, 0);
+                    image1.SetPixel(x, y, newColor);
+                }
+            }
+            pictureBox1.Image = image1;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Bitmap image1 = (Bitmap)openImg.Clone(); //make a copy
+            for (int x = 0; x < openImg.Width; x++)
+            {
+                for (int y = 0; y < openImg.Height; y++)
+                {
+                    //get the B value
+                    Color pixelColor = image1.GetPixel(x, y);
+                    Color newColor = Color.FromArgb(0, 0, pixelColor.B);
+                    image1.SetPixel(x, y, newColor);
+                }
+            }
+            pictureBox1.Image = image1;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // use the average method from RGB to grayscale
+            Bitmap image1 = (Bitmap)openImg.Clone(); //make a copy
+            for (int x = 0; x < openImg.Width; x++)
+            {
+                for (int y = 0; y < openImg.Height; y++)
+                {
+                    Color pixelColor = image1.GetPixel(x, y);
+                    int r = pixelColor.R;
+                    int g = pixelColor.G;
+                    int b = pixelColor.B;
+                    int avg = (r + g + b) / 3;
+                    image1.SetPixel(x, y, Color.FromArgb(avg, avg, avg));
+                }
+            }
+            pictureBox1.Image = image1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // implement mean filter
+            Bitmap image1 = (Bitmap)openImg.Clone(); //make a copy
+            for (int x = 1; x < openImg.Width-1; x++)
+            {
+                for (int y = 1; y < openImg.Height-1; y++)
+                {
+                    int mf = 0;
+                    // 3*3 filter
+                    for(int mx = -1; mx < 2; mx++)
+                    {
+                        for(int my = -1; my < 2; my++)
+                        {
+                            Color pixelColor = image1.GetPixel(x+mx, y+my);
+                            mf += pixelColor.G;
+                        }
+                    }
+                    image1.SetPixel(x, y, Color.FromArgb(mf/9, mf/9, mf/9));
+                }
+            }
+            pictureBox1.Image = image1;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            // implement median filter
+            Bitmap image1 = (Bitmap)openImg.Clone(); //make a copy
+            for (int x = 1; x < openImg.Width - 1; x++)
+            {
+                for (int y = 1; y < openImg.Height - 1; y++)
+                {
+                    int [] median_f = new int[9];
+                    int index = 0;
+                    //3*3 filter
+                    for (int mx = -1; mx < 2; mx++)
+                    {
+                        for (int my = -1; my < 2; my++)
+                        {
+                            median_f[index++] = image1.GetPixel(x + mx, y + my).R;
+                        }
+                    }
+                    Array.Sort(median_f);
+                    image1.SetPixel(x, y, Color.FromArgb(median_f[4], median_f[4], median_f[4]));
+                }
+            }
+            pictureBox1.Image = image1;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Bitmap image1 = (Bitmap)openImg.Clone(); //make a copy
+            var ChartArea1 = chart1.ChartAreas.First();
+            string[] xvalues = new string[256];
+            for (int i = 0; i < 256; i++)
+            {
+                xvalues[i] = i.ToString();
+            }
+            var objSeries = chart1.Series.First();
+            objSeries.Points.DataBindXY(xvalues, getpmf(image1));
+            pictureBox1.Image = image1;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            
+            int rmax = 0, rmin = 0, gmax = 0, gmin = 0, bmax = 0, bmin = 0;
+            Bitmap image1 = (Bitmap)openImg.Clone(); //make a copy
+            double[] numr = new double[256];
+            double[] numg = new double[256];
+            double[] numb = new double[256];
+            for (int x = 0; x < openImg.Width; x++)
+            {
+                for (int y = 0; y < openImg.Height; y++)
+                {
+                    Color pixelColor = image1.GetPixel(x, y);
+                    int r = pixelColor.R;
+                    int g = pixelColor.G;
+                    int b = pixelColor.B;
+                    rmax = Math.Max(rmax, r);
+                    rmin = Math.Min(rmin, r);
+                    gmax = Math.Max(gmax, g);
+                    gmin = Math.Min(gmin, g);
+                    bmax = Math.Max(bmax, b);
+                    bmin = Math.Min(bmin, b);
+                    numr[r]++;
+                    numg[g]++;
+                    numb[b]++;
+                }
+            }
+
+            for (int k = 0; k < 256; k++)// make the pmf function 
+            {
+                double value1 = numr[k];
+                double value2 = numg[k];
+                double value3 = numb[k];
+                double rate1 = value1 / (openImg.Height * openImg.Width * 1.0);
+                double rate2 = value2/ (openImg.Height * openImg.Width * 1.0);
+                double rate3 = value3/ (openImg.Height * openImg.Width * 1.0);
+                numr[k] = rate1;
+                numg[k] = rate2;
+                numb[k] = rate3;
+            }
+            for (int k = 1; k < 256; k++)// make the cdf function
+            {
+                numr[k] = numr[k] + numr[k - 1];
+                numg[k] = numg[k] + numg[k - 1];
+                numb[k] = numb[k] + numb[k - 1];
+            }
+            
+            for (int x = 0; x < openImg.Width; x++)
+            {
+                for (int y = 0; y < openImg.Height; y++)
+                {
+                    Color pixelColor = image1.GetPixel(x, y);
+                    int r = pixelColor.R;
+                    int g = pixelColor.G;
+                    int b = pixelColor.B;
+                    byte after_r = (byte)Math.Round((rmax-rmin)* numr[r]);
+                    byte after_g = (byte)Math.Round((gmax - gmin) * numg[g]);
+                    byte after_b = (byte)Math.Round((bmax - bmin) * numb[b]);
+                    Color newValue = Color.FromArgb(after_r, after_g, after_b);
+                    image1.SetPixel(x, y, newValue);
+                }
+            }
+            pictureBox1.Image = image1;
+            var ChartArea1 = chart1.ChartAreas.First();
+            string[] xvalues = new string[256];
+            for (int i = 0; i < 256; i++)
+            {
+                xvalues[i] = i.ToString();
+            }
+            var objSeries = chart1.Series.First();
+            objSeries.Points.DataBindXY(xvalues,getpmf(image1 ) );
+        }
+        double [] getpmf(Bitmap img) // to get the pmf array
+        {
+            double[] numr = new double[256];
+            for (int x = 0; x < img.Width; x++)
+            {
+                for (int y = 0; y < img.Height; y++)
+                {
+                    Color pixelColor = img.GetPixel(x, y);
+                    int r = pixelColor.R;
+                    numr[r]++;
+                }
+            }
+            for (int k = 0; k < 256; k++)// make the pmf function 
+            {
+                double value1 = numr[k];
+                double rate1 = value1 / (openImg.Height * openImg.Width * 1.0);
+                numr[k] = rate1;
+            }
+            return numr;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e) 
+        {
+            //implement threshold function
+            int threshold = Int32.Parse(textBox1.Text);
+            Bitmap image1 = (Bitmap)openImg.Clone(); //make a copy
+            for (int x = 0; x < openImg.Width; x++)
+            {
+                for (int y = 0; y < openImg.Height; y++)
+                {
+                    Color pixelColor = image1.GetPixel(x, y);
+                    int r = pixelColor.R > threshold ? 255 : 0;
+                    Color newValue = Color.FromArgb(r, r, r);
+                    image1.SetPixel(x, y, newValue);
+                }
+            }
+            pictureBox1.Image = image1;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            int[,] mask1 ={
+                    { -1,0,1 },
+                    { -2,0,2 },
+                    { -1,0,1 }
+            };
+            Bitmap image1 = (Bitmap)openImg.Clone(); //make a copy
+            for (int x = 1; x < openImg.Width-1; x++)
+            {
+                for (int y = 1; y < openImg.Height-1; y++)
+                {
+                    int sob_v = 0;
+                    Color pixelColor = image1.GetPixel(x, y);
+                    for (int mx = -1; mx < 2; mx++)
+                    {
+                        for (int my = -1; my < 2; my++)
+                        {
+                            sob_v += image1.GetPixel(x + mx, y + my).R*mask1[mx+1,my+1];
+                        }
+                    }
+                    sob_v = sob_v > 255 ? 255 : sob_v;
+                    sob_v = sob_v < 0 ? 0 : sob_v;
+                    image1.SetPixel(x, y, Color.FromArgb(sob_v, sob_v, sob_v));
+                }
+            }
+            pictureBox1.Image = image1;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            int[,] mask1 ={
+                    { -1,-2,-1},
+                    { 0,0,0   },
+                    { 1,2,1   }
+            };
+            Bitmap image1 = (Bitmap)openImg.Clone(); //make a copy
+            for (int x = 1; x < openImg.Width - 1; x++)
+            {
+                for (int y = 1; y < openImg.Height - 1; y++)
+                {
+                    int sob_h = 0;
+                    Color pixelColor = image1.GetPixel(x, y);
+                    for (int mx = -1; mx < 2; mx++)
+                    {
+                        for (int my = -1; my < 2; my++)
+                        {
+                            sob_h += image1.GetPixel(x + mx, y + my).R * mask1[mx + 1, my + 1];
+                        }
+                    }
+                    sob_h = sob_h > 255 ? 255 : sob_h;
+                    sob_h = sob_h < 0 ? 0 : sob_h;
+                    image1.SetPixel(x, y, Color.FromArgb(sob_h, sob_h, sob_h));
+                }
+            }
+            pictureBox1.Image = image1;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            int[,] mask1 ={     //horizontal
+                    { -1,-2,-1},
+                    { 0,0,0   },
+                    { 1,2,1   }
+            };
+            int[,] mask2 ={     //vertical
+                    { -1,0,1 },
+                    { -2,0,2 },
+                    { -1,0,1 }
+            };
+            Bitmap image1 = (Bitmap)openImg.Clone(); //make a copy
+            for (int x = 1; x < openImg.Width - 1; x++)
+            {
+                for (int y = 1; y < openImg.Height - 1; y++)
+                {
+                    int sob_h = 0;
+                    int sob_v = 0;
+                    Color pixelColor = image1.GetPixel(x, y);
+                    for (int mx = -1; mx < 2; mx++)
+                    {
+                        for (int my = -1; my < 2; my++)
+                        {
+                            sob_h += image1.GetPixel(x + mx, y + my).R * mask1[mx + 1, my + 1];
+                            sob_v += image1.GetPixel(x + mx, y + my).R * mask2[mx + 1, my + 1];
+                        }
+                    }
+                    int value = (int)Math.Pow((sob_h * sob_h + sob_v * sob_v), 0.5);
+                    value = value > 255 ? 255 : value;
+                    value = value < 0 ? 0 : value;
+                    image1.SetPixel(x, y, Color.FromArgb(value, value, value));
+                }
+            }
+            pictureBox1.Image = image1;
+        }
+
+    }
+}
